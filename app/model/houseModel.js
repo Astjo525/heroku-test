e083846567d1529b463ff3316fa1e54e99a89d1a
Astrid <astrid.c.johansson@gmail.com>
2020-04-15 11:19:46 +0200

    Ändrade tillbaka till första standardhustabell

--- app/model/houseModel.js
+++ app/model/houseModel.js
@@ -33,13 +33,14 @@ House.getAll = result => {
 
 //Retrieving all houses from the "houses" table
 House.getAllStandard = result => {
-    sql.query("SELECT house_id, collection_name, house_name, rooms, " +
-    "floors, living_area, build_area, height, roof_angle, roof_type_name, " +
-    "short_info, long_info, house_image, house_model " +
-    "FROM standard_houses " +
-    "INNER JOIN collections USING (collection_id) " +
-    "INNER JOIN roof_types USING (roof_type_id) " +
-    "ORDER BY house_name", (err, res) => {
+    sql.query("SELECT * FROM standardhouses"), (err, res) => {
+    // sql.query("SELECT house_id, collection_name, house_name, rooms, " +
+    // "floors, living_area, build_area, height, roof_angle, roof_type_name, " +
+    // "short_info, long_info, house_image, house_model " +
+    // "FROM standard_houses " +
+    // "INNER JOIN collections USING (collection_id) " +
+    // "INNER JOIN roof_types USING (roof_type_id) " +
+    // "ORDER BY house_name", (err, res) => {
         if(err) {
             result(err, null);
             return;
