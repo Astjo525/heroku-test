0c38c83688dc87346cfa82467314a301802e6888
Astrid <astrid.c.johansson@gmail.com>
2020-04-29 09:00:34 +0200

    Gick tillbaka till det tidigare

--- server.js
+++ server.js
@@ -26,36 +26,6 @@ app.get('/', (req, res) => {
 
 require("./app/routes/houseRoutes.js")(app);
 
-//MERGED DB
-require("dotenv").config();
-const session = require('express-session');
-const pageRouter = require('./routes/pages');
-const TWO_HOURS = 1000*60*60*2;
-
-//Express Session Middleware
-app.use(session({
-    name: "sid", //process.env.SESS_NAME,
-    resave: false,
-    saveUninitialized: false,
-    secret: "abc123", //process.env.SESS_SECRET,
-
-    cookie: {
-        maxAge: TWO_HOURS, 
-        samSite: true, 
-        secure: process.env.NODE_ENV === 'production' 
-        //TODO: secure: enable https - enable TLS connection to server 
-	}
-}));
-
-require("./routes/pages.js")(app);
-
-app.use(express.json());
-
-//routes
-app.use('/', pageRouter);
-
-//SLUT MERGED DB
-
 app.listen(port, () => {
     console.log("Connected to port " + port);
 });
