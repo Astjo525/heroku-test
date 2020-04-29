fca161c221c3a19daca1f1ad1e721ddd5b8a2a22
Astrid <astrid.c.johansson@gmail.com>
2020-04-29 15:40:23 +0200

    express-mysql-session fix

--- app.js
+++ app.js
@@ -11,49 +11,48 @@ const port = process.env.PORT || 3000;
 
 //express-mysql-session
 //EXPRESS-MYSQL-SESSION
+//EXPRESS-MYSQL-SESSION
 var MySQLStore = require('express-mysql-session')(session);
-var db = require('./db.js');
-var mysql = require('mysql');
 
-// var options = {
-//     host: "eu-cdbr-west-02.cleardb.net",
-//     port: port,
-//     user: "bf6652f839f5d9",
-//     password: "4eead042",
-//     database: "heroku_af4f9f69f37b397",
-//     clearExpired: true,
-//     checkExpirationInterval: 900000,
-//     expiration: 86400000,
-//     createDatabaseTable: true,
-//     connectionLimit: 1,
-//     endConnectionOnClose: true
-// }
-// //var connection = mysql.createConnection(db);
-// var sessionStore = new MySQLStore(options);
+var options = {
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "",
+    database: "merged_db",
+    // clearExpired: true,
+    // checkExpirationInterval: 900000,
+    // expiration: 86400000,
+    // createDatabaseTable: true,
+    // connectionLimit: 1,
+    // endConnectionOnClose: true
+}
 
-// //Express Session Middleware
-// app.use(session({
-//     key: "sid", //process.env.SESS_NAME,
-//     secret: "abc123", //process.env.SESS_SECRET,
-//     store: sessionStore,
-//     resave: false,
-//     saveUninitialized: false
-// }));
+var sessionStore = new MySQLStore(options);
 
-// //Express Session Middleware
- app.use(session({
-    name: "sid", //process.env.SESS_NAME,
-    resave: false,
-    saveUninitialized: false,
+//Express Session Middleware
+app.use(session({
+    key: "sid", //process.env.SESS_NAME,
     secret: "abc123", //process.env.SESS_SECRET,
-
-    cookie: {
-        maxAge: TWO_HOURS, 
-        samSite: true, 
-        secure: process.env.NODE_ENV === 'production' 
-        //TODO: secure: enable https - enable TLS connection to server 
-	}
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
 }));
+
+// //Express Session Middleware
+//  app.use(session({
+//     name: "sid", //process.env.SESS_NAME,
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: "abc123", //process.env.SESS_SECRET,
+
+//     cookie: {
+//         maxAge: TWO_HOURS, 
+//         samSite: true, 
+//         secure: process.env.NODE_ENV === 'production' 
+//         //TODO: secure: enable https - enable TLS connection to server 
+// 	}
+// }));
 
 app.use(express.json());
 
