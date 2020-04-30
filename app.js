fa174d90aa319dcc6a6d611b85c602f1f373eba8
Astrid <astrid.c.johansson@gmail.com>
2020-04-30 09:50:41 +0200

    header stuff

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(session({
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin, "*");
     res.setHeader('Access-Control-Allow-Credentials','true');
     res.setHeader('Access-Control-Allow-Headers','Origin, Content-type, Accept');
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, PUT, DELETE, HEAD, OPTIONS');
